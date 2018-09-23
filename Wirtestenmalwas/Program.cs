527ff9be999f0a21d60bfc48bc3f221d35128b8a
Peter Rüther <Peter.Ruether17@outlook.de>
2018-09-23 19:19:22 +0200

    jep einchecken

--- Wirtestenmalwas/Program.cs
+++ Wirtestenmalwas/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace Wirtestenmalwas
 {
     class Program
     {
         static void Main(string[] args)
+    
         {
+            int y = 1;
+            string z = "Hallo Ihr";
+            while(y < 10)  {
+                    Ausgabe(z, y);
+                   y = Eingabe(y);
+                            }
+            Console.ReadKey();
+            
+        }
+
+        static int  Eingabe(int x)
+        {
+            int result;
+            result = x + 1;
+            return result;
+        }
+
+        static void Ausgabe(string x, int y)
+        {
+
+
+            Console.WriteLine( x + y);
+            
+            
+            
         }
     }
+
 }
